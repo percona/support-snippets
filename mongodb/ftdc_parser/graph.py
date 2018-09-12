5e5b67aaf81dade9201729c3b5cbfe79642253c7
Adamo Tonete <adamobr@gmail.com>
2018-09-12 11:00:10 -0300

    ftdc parser

---
+++ mongodb/ftdc_parser/graph.py
@@ -0,0 +1,52 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+matplotlib.rc('figure', figsize=(100, 10))
+
+
+# Data for plotting
+def create_graph(_x, _y, _title, _metric, _filename):
+
+  y = _y[2:]
+  x = _x
+
+  x_array = []
+  y_array = []
+
+  #x_array.append('start')
+  for item in x:
+    if item <> '': x_array.append(item[-5:]);
+  for item in y:
+    if item <> '': y_array.append(int(item));
+
+  #print x_array
+  #print y_array
+  print x_array
+  print y_array
+  fig, ax = plt.subplots()
+  ax.plot(x_array, y_array)
+
+  ax.set(xlabel='Time (s)', ylabel=_metric,title=_title)
+  ax.grid()
+
+  fig.savefig(_filename)
+
+
+
+
+xx = []
+yy = []
+linecount = 0
+for line in sys.stdin:
+  newline =  line.replace('\n','');
+  if 'Timestamps' in newline:
+    xx = newline[11:]
+    xx = xx.split(';')
+  else:
+    yy = newline.split(';')
+    if len(xx) > 1:
+      print 'Generating ' + str(yy[:1])
+      create_graph(xx,yy,'Test',str(yy[:1][0]), str(yy[:1][0]).replace('.','_'))

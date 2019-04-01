83178df2c560b7d07c4e535e2b51f9ccce3f7387
Przemek <przemek@mysqlmaniac.com>
2019-04-01 10:03:16 +0200

    removing files usage and bc, using bash functions only now

--- mysql/pxc-scripts/uuid_to_gtid.sh
+++ mysql/pxc-scripts/uuid_to_gtid.sh
@@ -5,16 +5,16 @@
 # take the cluster UUID from wsrep_cluster_state_uuid status variable, like:
 # bash uuid_to_gtid.sh $(mysql -BN -e "select VARIABLE_VALUE from performance_schema.global_status where VARIABLE_NAME='wsrep_cluster_state_uuid'")
 
-rm -fr /tmp/myHEX?
-
-for i in {1..5}; 
-		do echo $1|awk -F '-' "{print toupper(\$$i)}" > /tmp/myHEX$i 
+IFS=$'-';
+arr=()
+for i in $1; do 
+	arr+=($i); 
 done
 
-	a1=$(echo "obase=16;ibase=16;FFFFFFFF-$(cat /tmp/myHEX1)"|bc)
-	a2=$(echo "obase=16;ibase=16;FFFF-$(cat /tmp/myHEX2)"|bc)
-	a3=$(echo "obase=16;ibase=16;FFFF-$(cat /tmp/myHEX3)"|bc)
-	a4=$(echo "obase=16;ibase=16;FFFF-$(cat /tmp/myHEX4)"|bc)
-	a5=$(echo "obase=16;ibase=16;FFFFFFFFFFFF-$(cat /tmp/myHEX5)"|bc)
+a1=$(printf '%x\n' $((0xFFFFFFFF - 0x${arr[0]})))
+a2=$(printf '%x\n' $((0xFFFF - 0x${arr[1]})))
+a3=$(printf '%x\n' $((0xFFFF - 0x${arr[2]})))
+a4=$(printf '%x\n' $((0xFFFF - 0x${arr[3]})))
+a5=$(printf '%x\n' $((0xFFFFFFFFFFFF - 0x${arr[4]})))
 
 echo $a1-$a2-$a3-$a4-$a5|tr '[:upper:]' '[:lower:]'

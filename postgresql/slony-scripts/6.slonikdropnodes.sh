badf0daf1e1c2841e3d79322bf4519eec4fff8cf
jobinau <jobinau@gmail.com>
2019-04-18 14:12:04 +0530

    sample scripts for slony setup

---
+++ postgresql/slony-scripts/6.slonikdropnodes.sh
@@ -0,0 +1,18 @@
+#!/usr/local/pgsql/bin/slonik
+define CLUSTER slony;
+define PRIMARY 1;
+define SLAVE 10;
+cluster name = @CLUSTER;
+
+node @PRIMARY admin conninfo = 'dbname=pgbench host=pgslony84 port=5432 user=postgres password=secret';
+
+node @SLAVE admin conninfo = 'dbname=pgbench host=pgslony11 port=5432 user=postgres password=secret';
+
+DROP NODE ( ID = @SLAVE, EVENT NODE = @PRIMARY );
+
+UNINSTALL NODE ( ID = @SLAVE);
+
+UNINSTALL NODE ( ID = @PRIMARY);
+
+
+

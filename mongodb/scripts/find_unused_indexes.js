9f0871fa0ba046a124d9aaf5f186b7a8214b7a0b
Jean Nunes <20301222+jenunes@users.noreply.github.com>
2023-12-21 14:44:25 -0300

    Update find_unused_indexes.js
    Last change (dd/mm/yyyy): 31/10/2023.
    What changed: Added the drop command, compatibility with sharded cluster, and formated the output.
    Editor: Jean da Silva.

--- mongodb/scripts/find_unused_indexes.js
+++ mongodb/scripts/find_unused_indexes.js
@@ -1,44 +1,49 @@
-// This script will print in the output indexes that never been used based on the access.
-// Added the drop command
+/* 
+Objective: This script will print in the output indexes that have never been used based on the access.
+Last change (dd/mm/yyyy): 31/10/2023.
+What changed: Added the drop command, compatibility with sharded cluster, and formated the output.
+Editor: Jean da Silva.
+*/
 
-var ldb = db.adminCommand( { listDatabases: 1 } ); 
+var ldb = db.adminCommand({ listDatabases: 1 });
 
-for ( i = 0; i < ldb.databases.length; i++ ) { 
-
-  if ( ldb.databases[i].name != 'admin' && ldb.databases[i].name != 'config' && ldb.databases[i].name != 'local') {
-    print('DATABASE ',ldb.databases[i].name); 
-    print("+++++++++++++++++++++++++++++++++++++++++");
-
-    var db = db.getSiblingDB(ldb.databases[i].name); 
-    var cpd = db.getCollectionNames(); 
-
-    for ( j = 0; j < cpd.length; j++ ) { 
-
-      if ( cpd[j] !=  'system.profile' ) { 
-
-        var indexstats = JSON.parse(JSON.stringify(db.runCommand( { aggregate : cpd[j], pipeline : [ { $indexStats: { } }, { $match: { "accesses.ops": 0 } } ], cursor: { batchSize: 1000 }  } ).cursor.firstBatch));
-
-        for ( k = 0; k < indexstats.length; k++ ) { 
-          if ( k == 0) {
-            print("Collection :"+cpd[j]);            
-          }
-
-          shardName = ((JSON.stringify(indexstats[k].shard)));
-          indexfield = ((JSON.stringify(indexstats[k].key)));
-          indexusage = ((JSON.stringify(indexstats[k].accesses.ops)));
-          indexes_name = (JSON.stringify(indexstats[k].name)).replace(/,/g ,"_");
-          
-          if (indexfield != '{"_id":1}') {
-            if(typeof shardName !== "undefined") {
-              print("   Shard : "  + shardName);
+for (i = 0; i < ldb.databases.length; i++) {
+    if (ldb.databases[i].name != 'admin' && ldb.databases[i].name != 'config' && ldb.databases[i].name != 'local') {
+        print('DATABASE ', ldb.databases[i].name);
+        print('+++++++++++++++++++++++++++++++++++++++++');
+        
+        var currentDb = db.getSiblingDB(ldb.databases[i].name);
+        var collections = currentDb.getCollectionNames();
+        var summarizedIndexes = {};
+        
+        for (j = 0; j < collections.length; j++) {
+            if (collections[j] != 'system.profile') {
+                var indexStats = currentDb.runCommand({ aggregate: collections[j], pipeline: [{ $indexStats: {} }], cursor: { batchSize: 1000 } }).cursor.firstBatch;
+                
+                for (k = 0; k < indexStats.length; k++) {
+                    var indexName = indexStats[k].name;
+                    var indexUsage = indexStats[k].accesses.ops;
+                    
+                    if (summarizedIndexes[indexName]) {
+                        summarizedIndexes[indexName].usage += indexUsage;
+                    } else {
+                        summarizedIndexes[indexName] = {
+                            usage: indexUsage,
+                            collection: collections[j]
+                        };
+                    }
+                }
             }
-            print("   Index name: "  + indexes_name);
-            print("   Index columns: " + indexfield + " ==> ",indexusage);
-            print("   Drop command: db.getSiblingDB('" + ldb.databases[i].name + "')." + cpd[j] + ".dropIndex(" + indexes_name + ")");
-            print("\n");
-          }
         }
-      } 
-    } 
-  }
+        
+      for (var index in summarizedIndexes) {
+            if (index !== '_id_' && summarizedIndexes[index].usage == 0) {
+              print(" Collection: ", summarizedIndexes[index].collection);
+                print("   Index name: ", index);
+                print("   Index access sum: ", summarizedIndexes[index].usage);
+              print("   Drop command: db.getSiblingDB('" + ldb.databases[i].name + "')." + summarizedIndexes[index].collection + ".dropIndex('" + index + "');");
+                print("\n");
+            }
+        }
+    }
 }

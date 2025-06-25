4d1d14529ff8ff97021a148572e8663450c840ca
Jean da Silva <jean_nsilva@hotmail.com>
2025-06-25 17:23:21 -0300

    Update calculate_cardinality_with_sampling.js
     Renamed the output from "counts", to "samples" for better understanding.

--- mongodb/scripts/calculate_cardinality_with_sampling.js
+++ mongodb/scripts/calculate_cardinality_with_sampling.js
@@ -1,29 +1,27 @@
+/* 
+Objective: This script will calculate the Cardinality for large collections from a sample.
+Last change (dd/mm/yyyy): 25/06/2025.
+What changed: Renamed the output from "counts" to "samples" for better understanding.
+Editor: Jean da Silva.
+*/
+
+
 db.getSiblingDB('<database-name>').getCollection('<collection-name>').aggregate([
-  { $sample: { size: 20000 } },
+  { $sample: { size: 10000 } },
   {
-      "$facet":{
-         "dist_count":[
-            {
-               "$group":{
-                  "_id":"$<field-name>"
-               }
-            }
-         ],
-         "count":[
-            {
-               "$count":"count"
-            }
-         ]
-      }
-   },
-   {
-      "$addFields":{
-         "dist_count":{
-            "$size":"$dist_count"
-         },
-         "count":{
-            "$first":"$count.count"
-         }
-      }
-   }
+    $facet: {
+      dist_count: [
+        { $group: { _id: "<field-name>" } }
+      ],
+      samples: [ // renamed from "count"
+        { $count: "samples" } // renamed count field to "samples"
+      ]
+    }
+  },
+  {
+    $addFields: {
+      dist_count: { $size: "$dist_count" },
+      samples: { $first: "$samples.samples" } // adjusted field reference
+    }
+  }
 ]);

6de37cf5a1f7c8c9241857d631c875c426ac32c2
Vinodh Krishnaswamy <35266499+vinokrishna@users.noreply.github.com>
2025-06-27 01:43:08 +0530

    Update calculate_cardinality_with_sampling.js
    added aggregate for finding cardinality for multiple fields

--- mongodb/scripts/calculate_cardinality_with_sampling.js
+++ mongodb/scripts/calculate_cardinality_with_sampling.js
@@ -25,3 +25,29 @@ db.getSiblingDB('<database-name>').getCollection('<collection-name>').aggregate(
     }
   }
 ]);
+
+/* 
+Objective: // adding multiple fields for finding cardinality for multiple fields when one intended to create compsite index
+Last change (dd/mm/yyyy): 27/06/2025.
+Editor: Vinodh Guruji.
+*/
+
+db.getSiblingDB('<database-name>').getCollection('<collection-name>').aggregate([
+  { $sample: { size: 10000 } },
+  {
+    $facet: {
+      dist_count: [
+        { $group: { _id: {field1: "<field-name>", field2: "<field-name>", ..  } } }
+      ],
+      samples: [ // renamed from "count"
+        { $count: "samples" } // renamed count field to "samples"
+      ]
+    }
+  },
+  {
+    $addFields: {
+      dist_count: { $size: "$dist_count" },
+      samples: { $first: "$samples.samples" } // adjusted field reference
+    }
+  }
+]);
